b6da8017781da730790383bab4e3694aebfb7f56
Matthew Larson <matthewal194@gmail.com>
2026-05-11 12:00:13 -0700

    Starting work on sending commands

---
+++ frontend/src/Components/CommandSender.tsx
@@ -0,0 +1,13 @@
+import Nave from "./Nave";
+import "../index.css"
+
+function CommandSender(){
+    return (
+        <div className = "CommandSender">
+            hey
+        </div>
+    )
+}
+
+
+export default CommandSender
--- frontend/src/Components/Telemetry.tsx
+++ frontend/src/Components/Telemetry.tsx
@@ -5,6 +5,7 @@ import { useState, type RefObject } from "react";
 import DynamicPlot from "./DynamicPlot";
 import { useDarkMode } from "../context/DarkModeContaxt";
 import { DATA_TYPE_MAP, type DataTypeKey } from "../types/dataTypes";
+import CommandSender from "./CommandSender"
 
 // Types
 type Chart = {
@@ -117,7 +118,12 @@ export default function Telemetry() {
   return (
     <div style={{ padding: 16 }}>
       <div style={{ marginBottom: 16 }}>
+        <button >Send Command</button>
         <button onClick={addChart}>Add Plot</button>
+      </div>
+
+      <div>
+        <CommandSender></CommandSender>
       </div>
 
       <div
