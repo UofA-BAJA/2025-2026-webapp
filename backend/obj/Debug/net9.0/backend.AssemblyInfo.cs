489a39b16e541ecd602d8f0e84d9ea8e6b2a38ce
John Imanishimwe <148114742+JohnVianme@users.noreply.github.com>
2026-05-26 00:43:36 -0700

    unknown

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f06dee956aba787cc90e601e220567a02c45332b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bacc479f006e4b6a90ecb8c1391a3e0a6e44384")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
